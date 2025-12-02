1a78c4318a655170bec36b82753a0302e5dbfb25
ceren454 <cerebtoy@gmail.com>
2025-12-02 23:58:30 +0300

    Add ClassAverage program to calculate grades
    Implement a program to calculate class average based on user input grades.

---
+++ classaverage.cs
@@ -0,0 +1,30 @@
+using System;
+
+class ClassAverage
+{
+    static void Main()
+    {
+        int total = 0;
+        int gradeCounter = 0;
+
+        Console.Write("Enter grade or -1 to quit: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        while (grade != -1)
+        {
+            total += grade;
+            gradeCounter++;
+
+            Console.Write("Enter grade or -1 to quit: ");
+            grade = int.Parse(Console.ReadLine());
+        }
+
+        if (gradeCounter != 0)
+        {
+            double average = (double)total / gradeCounter;
+
+            Console.WriteLine($"\nTotal of the {gradeCounter} grades entered is {total}");
+            Console.WriteLine($"Class average is {average:F}");
+        }
+        else
+        {
